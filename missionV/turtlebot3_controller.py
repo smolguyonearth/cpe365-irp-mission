3a0f333ef60dbcd9511b3a19267ab4f755a48fb1
smolguyonearth <poynitis@gmail.com>
2025-09-23 10:29:06 +0700

    missionV-1

---
+++ missionV/turtlebot3_controller.py
@@ -0,0 +1,128 @@
+# Authors : Nopphakorn Subs. Niwatchai Wang. Supasate Wor. Narith Tha. Tawan Thaep. Napatharak Muan.
+from dis import dis
+from socket import TIPC_SUBSCR_TIMEOUT
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+
+from sensor_msgs.msg import LaserScan, BatteryState
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+
+from time import sleep
+#from std_msgs.msg import String
+
+class Turtlebot3Controller(Node):
+
+    def __init__(self):
+        super().__init__('turtlebot3_controller')   #node name
+        self.cmdVelPublisher = self.create_publisher(Twist, 'cmd_vel', 1)
+        self.scanSubscriber = self.create_subscription(LaserScan, 'scan', self.scanCallback, qos_profile=qos_profile_sensor_data)
+        self.batteryStateSubscriber = self.create_subscription(BatteryState, 'battery_state', self.batteryStateCallback, 1)
+        self.odomSubscriber = self.create_subscription(Odometry, 'odom', self.odomCallback, 1)
+        self.valueLaserRaw = {
+            'range_min':0.0,
+            'range_max':0.0,
+            'ranges':[0.0]*360,
+        }
+        self.valueBatteryState = None
+        self.valueOdometry = {
+            'position':None,        #Datatype: geometry_msg/Point   (x,y,z)
+            'orientation':None,     #Datatype: geometry_msg/Quaternion (x,y,z,w)
+            'linearVelocity':None,  #Datatype: geometry_msg/Vector3 (x,y,z)
+            'angularVelocity':None, #Datatype: geometry_msg/Vector3 (x,y,z)
+        }
+        
+        self.state = 0
+        #Use this timer for the job that should be looping until interrupted
+        self.timer = self.create_timer(0.1,self.timerCallback)
+
+    def publishVelocityCommand(self, linearVelocity, angularVelocity):
+        msg = Twist()
+        msg.linear.x = linearVelocity * 1.0
+        msg.angular.z = angularVelocity * 1.0
+        self.cmdVelPublisher.publish(msg)
+        #self.get_logger().info('Publishing cmd_vel: "%s", "%s"' % linearVelocity, angularVelocity)
+
+    def scanCallback(self, msg):
+        self.valueLaserRaw = {
+            'range_min':msg.range_min,
+            'range_max':msg.range_max,
+            'ranges':list(msg.ranges),
+        }
+
+    def batteryStateCallback(self, msg):
+        self.valueBatteryState = msg
+
+    def odomCallback(self, msg):
+        self.valueOdometry = {
+            'position':msg.pose.pose.position,
+            'orientation':msg.pose.pose.orientation,
+            'linearVelocity':msg.twist.twist.linear,
+            'angularVelocity':msg.twist.twist.angular,
+        }
+
+    def timerCallback(self):
+        print("-----------------------------------------------------------")
+        print('timer triggered')
+
+        threshold = 0.2
+        window = 20
+        directions = [0, 270, 180, 90]
+        pattern = []
+
+        for angle in directions:
+            start = (angle - window) % 360
+            end   = (angle + window) % 360
+
+            if start < end:
+                sector = self.valueLaserRaw['ranges'][start:end+1]
+            else:
+                sector = self.valueLaserRaw['ranges'][start:] + self.valueLaserRaw['ranges'][:end+1]
+
+            valid = [r for r in sector if r != 0.0]
+
+            if valid and min(valid) <= threshold:
+                pattern.append(1)
+            else:
+                pattern.append(0)
+
+        print(" ".join(map(str, pattern)))
+
+        if (pattern[0] == 1):
+            print("ahead\n")
+        elif(pattern[1] == 1):
+            print("right\n")
+        elif(pattern[2] == 1):
+            print("back\n")
+        elif(pattern[3] == 1):
+            print("left\n")
+
+
+
+def robotStop():
+    node = rclpy.create_node('tb3Stop')
+    publisher = node.create_publisher(Twist, 'cmd_vel', 1)
+    msg = Twist()
+    msg.linear.x = 0.0
+    msg.angular.z = 0.0
+    publisher.publish(msg)
+            
+
+def main(args=None):
+    rclpy.init(args=args)
+    tb3ControllerNode = Turtlebot3Controller()
+    print('tb3ControllerNode created')
+    try:
+        rclpy.spin(tb3ControllerNode)
+    except:
+        KeyboardInterrupt
+    print('Done')
+    
+    tb3ControllerNode.publishVelocityCommand(0.0,0.0)
+    tb3ControllerNode.destroy_node()
+    robotStop()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
